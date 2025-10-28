924c7b3386e95d71f6853b1462baafe4e19cfe52
Blank <wangfj11@foxmail.com>
2025-10-28 10:34:12 +0800

    feat: 添加Facebook登录功能组件及文档
    添加Facebook登录功能的核心组件，包括登录管理器、成功回调数据结构和编辑器扩展
    创建CHANGELOG和LICENSE文件，完善项目文档结构
    实现Facebook登录的初始化、登录和登出功能接口

---
+++ Runtime/FaceBookLoginComponent.cs
@@ -0,0 +1,63 @@
+﻿// GameFrameX 组织下的以及组织衍生的项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+// 
+// 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE 文件。
+// 
+// 不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目二次开发而产生的一切法律纠纷和责任，我们不承担任何责任！
+
+using System;
+using GameFrameX.Runtime;
+using UnityEngine;
+
+namespace GameFrameX.Login.FaceBook.Runtime
+{
+    /// <summary>
+    /// FaceBook登录组件。
+    /// </summary>
+    [DisallowMultipleComponent]
+    [AddComponentMenu("Game Framework/FaceBook Login")]
+    [RequireComponent(typeof(GameFrameXFaceBookLoginCroppingHelper))]
+    [UnityEngine.Scripting.Preserve]
+    public class FaceBookLoginComponent : GameFrameworkComponent
+    {
+        private IFaceBookLoginManager _FaceBookLoginManager = null;
+
+        [SerializeField] private string m_AppId = string.Empty;
+
+        [SerializeField] private string m_AppKey = string.Empty;
+
+        /// <summary>
+        /// 游戏框架组件初始化。
+        /// </summary>
+        protected override void Awake()
+        {
+            ImplementationComponentType = Utility.Assembly.GetType(componentType);
+            InterfaceComponentType = typeof(IFaceBookLoginManager);
+            base.Awake();
+
+            _FaceBookLoginManager = GameFrameworkEntry.GetModule<IFaceBookLoginManager>();
+            if (_FaceBookLoginManager == null)
+            {
+                Log.Fatal("face book manager is invalid.");
+                return;
+            }
+        }
+
+        [UnityEngine.Scripting.Preserve]
+        public void Init()
+        {
+            _FaceBookLoginManager.Init(m_AppId, m_AppKey);
+        }
+
+        [UnityEngine.Scripting.Preserve]
+        public void Login(Action<FaceBookLoginSuccess> loginSuccess, Action<int> loginFail)
+        {
+            _FaceBookLoginManager.Login(loginSuccess, loginFail);
+        }
+
+        [UnityEngine.Scripting.Preserve]
+        public void LogOut()
+        {
+            _FaceBookLoginManager.LogOut();
+        }
+    }
+}
---
+++ Runtime/FaceBookLoginManager.cs
@@ -0,0 +1,113 @@
+﻿// GameFrameX 组织下的以及组织衍生的项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+// 
+// 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE 文件。
+// 
+// 不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目二次开发而产生的一切法律纠纷和责任，我们不承担任何责任！
+
+using System;
+using cn.sharesdk.unity3d;
+using GameFrameX.Event.Runtime;
+using GameFrameX.Runtime;
+using GameFrameX.ShareSdk.Runtime;
+using UnityEngine;
+
+namespace GameFrameX.Login.FaceBook.Runtime
+{
+    [UnityEngine.Scripting.Preserve]
+    public sealed class FaceBookLoginManager : GameFrameworkModule, IFaceBookLoginManager
+    {
+        [UnityEngine.Scripting.Preserve]
+        public FaceBookLoginManager()
+        {
+        }
+
+        private EventComponent _eventComponent;
+        private ShareSDK _shareSDK;
+
+        [UnityEngine.Scripting.Preserve]
+        public void Init(string appId, string appKey)
+        {
+            _eventComponent = GameEntry.GetComponent<EventComponent>();
+            _eventComponent.CheckSubscribe(AuthEventArgs.EventId, OnAuthEventArgs);
+            _shareSDK = UnityEngine.Object.FindObjectOfType<ShareSDK>();
+            _shareSDK.devInfo.facebook.ConsumerKey = appId;
+            _shareSDK.devInfo.facebook.ConsumerSecret = appKey;
+
+            _shareSDK.devInfo.facebookAccount.AppKey = appId;
+            _shareSDK.devInfo.facebookAccount.AppSecret = appKey;
+
+            _shareSDK.devInfo.facebookMessenger.AppId = appId;
+        }
+
+        private void OnAuthEventArgs(object sender, GameEventArgs e)
+        {
+            if (e is AuthEventArgs eventArgs)
+            {
+                if (eventArgs.Type != PlatformType.Facebook)
+                {
+                    return;
+                }
+
+                if (eventArgs.State == ResponseState.Success)
+                {
+                    var faceBookLoginSuccess = new FaceBookLoginSuccess();
+                    if (eventArgs.Data.ContainsKey("name"))
+                    {
+                        faceBookLoginSuccess.Name = eventArgs.Data["name"].ToString();
+                    }
+
+                    if (eventArgs.Data.ContainsKey("id"))
+                    {
+                        faceBookLoginSuccess.Id = eventArgs.Data["id"].ToString();
+                    }
+
+                    if (eventArgs.Data.ContainsKey("uid"))
+                    {
+                        faceBookLoginSuccess.Uid = eventArgs.Data["uid"].ToString();
+                    }
+
+                    if (eventArgs.Data.ContainsKey("picture"))
+                    {
+                        faceBookLoginSuccess.PhotoUrl = eventArgs.Data["picture"].ToString();
+                    }
+
+                    if (eventArgs.Data.ContainsKey("email"))
+                    {
+                        faceBookLoginSuccess.Email = eventArgs.Data["email"].ToString();
+                    }
+
+                    _loginSuccess.Invoke(faceBookLoginSuccess);
+                }
+                else
+                {
+                    _loginFail.Invoke((int)eventArgs.State);
+                }
+            }
+        }
+
+        private Action<FaceBookLoginSuccess> _loginSuccess;
+        private Action<int> _loginFail;
+
+        [UnityEngine.Scripting.Preserve]
+        public void Login(Action<FaceBookLoginSuccess> loginSuccess, Action<int> loginFail)
+        {
+            _loginSuccess = loginSuccess;
+            _loginFail = loginFail;
+            _shareSDK.Authorize(PlatformType.Facebook);
+        }
+
+        [UnityEngine.Scripting.Preserve]
+        public void LogOut()
+        {
+            _shareSDK.CancelAuthorize(PlatformType.Facebook);
+        }
+
+        protected override void Update(float elapseSeconds, float realElapseSeconds)
+        {
+        }
+
+        protected override void Shutdown()
+        {
+        }
+    }
+}
